11c4254420511d3c61097d7fde69f8e1b29ba931
Melvin Carvalho <melvincarvalho@gmail.com>
2016-02-07 00:32:40 +0100

    Append twtxt.txt

--- tweet.js
+++ tweet.js
@@ -1,7 +1,19 @@
+var fs = require('fs');
+
 var description = process.argv[2];
 
 var now = new Date().toISOString();
 
-var output = now + "\t" + description;
+var output = now + "\t" + description + '\n';
 
 console.log(output);
+
+var HOME = process.env['HOME'];
+
+var twtfile =  HOME + '/twtxt.txt';
+
+fs.appendFile(twtfile, output, function (err) {
+  if (err) {
+    console.error(err);
+  }
+});
