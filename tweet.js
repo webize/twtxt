1aa91192c0fc10c992e9869558a0ddf1dd626a6d
Melvin Carvalho <melvincarvalho@gmail.com>
2016-02-07 00:46:11 +0100

    Add validation

--- tweet.js
+++ tweet.js
@@ -1,16 +1,27 @@
+// requires
 var fs = require('fs');
+
+// init
+var MAXCHARS = 140;
+var HOME     = process.env.HOME;
+var twtfile  =  HOME + '/twtxt.txt';
+var now      = new Date().toISOString();
 
 var description = process.argv[2];
 
-var now = new Date().toISOString();
+if (!description) {
+  console.error('Usage: twtxt <text>');
+  process.exit(-1);
+}
+
+if (description && description.length > MAXCHARS) {
+  console.error('Maximum tweet length : ' + MAXCHARS);
+  process.exit(-1);
+}
+
 
 var output = now + "\t" + description + '\n';
-
-console.log(output);
-
-var HOME = process.env['HOME'];
-
-var twtfile =  HOME + '/twtxt.txt';
+console.log('blogging : ' + output);
 
 fs.appendFile(twtfile, output, function (err) {
   if (err) {
