0ee5013d049fa9af78e638eb6bbfd64ba29e869a
Melvin Carvalho <melvincarvalho@gmail.com>
2016-02-07 16:52:43 +0100

    Starting with tests

---
+++ test/helper.js
@@ -0,0 +1,8 @@
+/* test/my_test.js */
+var expect = require('chai').expect;
+
+describe('sanity test of helper', function () {
+  it('passes', function () {
+    expect(true).to.eql(true);
+  });
+});
