c54eb4733c0b8616acbb8b9990a811687d301fc7
Melvin Carvalho <melvincarvalho@gmail.com>
2016-02-07 18:11:12 +0100

    Add config lib

--- bin/twtxt.js
+++ bin/twtxt.js
@@ -9,53 +9,19 @@ var childProcess = require('child_process');
 var readline     = require('readline');
 var moment       = require('moment');
 
+var hook         = require("../lib/helper").hook;
+var getUserHome  = require("../lib/helper").getUserHome;
+var getUserName  = require("../lib/helper").getUserName;
+
+var getUserName  = require("../lib/config").getUserName;
+
+
 var error   = debug('app:error');
 
 // defaults
 var default_limit_timeline = 20;
 
-
-// helper functions
-
-/**
-* hook
-* @param  {String} hook run a hook
-*/
-function hook(command) {
-  var proc = childProcess.exec(command, function (error, stdout, stderr) {
-    if (error) {
-      console.error(error.stack);
-      console.error('Error code: '+error.code);
-      console.error('Signal received: '+error.signal);
-    }
-    debug('Child Process STDOUT: '+stdout);
-    console.error('Child Process STDERR: '+stderr);
-  });
-
-  proc.on('exit', function (code) {
-    debug('Child process exited with exit code '+code);
-  });
-}
-
-
-
 // config functions
-
-/**
-* getUserHome
-* @return {String} home directory
-*/
-function getUserHome() {
-  return process.env[(process.platform == 'win32') ? 'USERPROFILE' : 'HOME'];
-}
-
-/**
-* getUserName
-* @return {String} user name
-*/
-function getUserName() {
-  return process.env.USER;
-}
 
 /**
 * gets the default config file
---
+++ test/config.js
@@ -0,0 +1,20 @@
+var config = require("../lib/config");
+
+/* test/my_test.js */
+var expect = require('chai').expect;
+
+describe('Config Functions', function () {
+
+  describe('getConfigFile', function() {
+    it('getConfigFile is a function', function () {
+      expect( (config.getConfigFile)).to.be.a('function');
+    });
+  });
+
+  describe('getConfig', function() {
+    it('getConfig is a function', function () {
+      expect( (config.getConfig)).to.be.a('function');
+    });
+  });
+
+});
