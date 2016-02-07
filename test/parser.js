4205c1a00b5f0c71bce7ef7068870ca7fbf2f5b4
Melvin Carvalho <melvincarvalho@gmail.com>
2016-02-07 21:03:00 +0100

    Add test

--- test/parser.js
+++ test/parser.js
@@ -9,6 +9,9 @@ describe('Parser Functions', function () {
     it('serializeTweet is a function', function () {
       expect( (parser.serializeTweet)).to.be.a('function');
     });
+    it('serializeTweet gives the right output', function () {
+      expect(parser.serializeTweet('test', '2016-02-07T19:55:58.254Z')).to.equal('2016-02-07T19:55:58.254Z\ttest\n');
+    });
   });
 
   describe('parseTimeline', function() {
