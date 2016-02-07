807039f1c3eda38139fb75a96a69980c035764b3
Melvin Carvalho <melvincarvalho@gmail.com>
2016-02-07 01:11:37 +0100

    Add functions

--- tweet.js
+++ tweet.js
@@ -1,30 +1,51 @@
 // requires
 var fs = require('fs');
 
-// init
-var MAXCHARS = 140;
-var HOME     = process.env.HOME;
-var twtfile  =  HOME + '/twtxt.txt';
-var now      = new Date().toISOString();
+// config functions
 
-var description = process.argv[2];
-
-if (!description) {
-  console.error('Usage: twtxt <text>');
-  process.exit(-1);
-}
-
-if (description && description.length > MAXCHARS) {
-  console.error('Maximum tweet length : ' + MAXCHARS);
-  process.exit(-1);
+/**
+ * getUserHome
+ * @return {String} home directory
+ */
+function getUserHome() {
+  return process.env[(process.platform == 'win32') ? 'USERPROFILE' : 'HOME'];
 }
 
 
-var output = now + "\t" + description + '\n';
-console.log('blogging : ' + output);
+// twtxt functions
+/**
+ * Append a new tweet to your twtxt file.
+ * @return {[type]} [description]
+ */
+function tweet() {
+  // init
+  var MAXCHARS = 140;
+  var HOME     = getUserHome();
+  var twtfile  = HOME + '/twtxt.txt';
+  var now      = new Date().toISOString();
 
-fs.appendFile(twtfile, output, function (err) {
-  if (err) {
-    console.error(err);
+  var description = process.argv[2];
+
+  if (!description) {
+    console.error('Usage: twtxt <text>');
+    process.exit(-1);
   }
-});
+
+  if (description && description.length > MAXCHARS) {
+    console.error('Maximum tweet length : ' + MAXCHARS);
+    process.exit(-1);
+  }
+
+
+  var output = now + "\t" + description + '\n';
+  console.log('blogging : ' + output);
+
+  fs.appendFile(twtfile, output, function (err) {
+    if (err) {
+      console.error(err);
+    }
+  });
+}
+
+
+tweet();
